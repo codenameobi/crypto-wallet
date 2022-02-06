4f79b129c60b7cd67cd9d52f3feddf92a5babd16
Obiora Nwude <44972479+datcodeboy@users.noreply.github.com>
2022-02-06 11:43:53 +0000

    added authenation

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
 // ignore_for_file: prefer_const_constructors
 
+import 'package:crypto_wallet/model/coin.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
@@ -12,22 +16,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      home: MyHomePage(),
+      title: 'Crypto Wallet',
+      home: Authentication(),
     );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key}) : super(key: key);
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(body: Container());
   }
 }

2df385490c096911c0052a5a6be0688b9f9ec709
Obiora Nwude <44972479+datcodeboy@users.noreply.github.com>
2022-02-09 23:06:48 +0000

    include firestore add function

--- lib/ui/add_view.dart
+++ lib/ui/add_view.dart
@@ -1,3 +1,4 @@
+import 'package:crypto_wallet/services/flutterfire.dart';
 import 'package:flutter/material.dart';
 
 class AddView extends StatefulWidget {
@@ -49,7 +50,7 @@ class _AddViewState extends State<AddView> {
               ),
               child: MaterialButton(
                 onPressed: () async {
-                  // TODO
+                  await addCoin(dropdownValue, _amountController.text);
                   Navigator.of(context).pop();
                 },
                 child: Text("Add"),
